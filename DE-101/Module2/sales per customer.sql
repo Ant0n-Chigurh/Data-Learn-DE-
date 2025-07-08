d2984974327035573b78a2eb9bcb0bd548b72bb6
Ant0n-Chigurh <backenddeveloper44356@gmail.com>
2025-07-08 15:03:16 +0300

    Delete DE-101/Module2 directory

--- DE-101/Module2/sales per customer.sql
+++
@@ -1,9 +0,0 @@
-create table sales_per_customer as (
-select 
-customer_id,
-customer_name,
-sum(sales) as sum_sales
-from orders
-where order_id not in (select order_id from returns)
-group by customer_id, customer_name
-order by sum_sales desc)
